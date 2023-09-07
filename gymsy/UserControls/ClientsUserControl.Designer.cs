d879a48559ed280eebe0115be5fda7a5c3b2d090
JereEG <jeremiasgonzalez7464@gmail.com>
2023-09-07 13:33:20 -0300

    se desinstalo bunifu

--- gymsy/UserControls/ClientsUserControl.Designer.cs
+++ gymsy/UserControls/ClientsUserControl.Designer.cs
@@ -29,26 +29,80 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
-            label1.Location = new Point(306, 185);
+            label1.Location = new Point(456, 250);
             label1.Name = "label1";
             label1.Size = new Size(100, 15);
             label1.TabIndex = 0;
             label1.Text = "ClientuserControl";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(23, 120);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(961, 385);
+            dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(644, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(212, 29);
+            textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 530);
+            panel1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top;
+            button2.BackColor = Color.DarkOrange;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(875, 27);
+            button2.Name = "button2";
+            button2.Padding = new Padding(10);
+            button2.Size = new Size(98, 50);
+            button2.TabIndex = 11;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = false;
             // 
             // ClientsUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
             Margin = new Padding(0);
             Name = "ClientsUserControl";
-            Size = new Size(700, 400);
+            Size = new Size(1000, 530);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +110,9 @@
         #endregion
 
         private Label label1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Button button2;
     }
 }
