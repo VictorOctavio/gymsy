dbab2ca84e522902488d2158a066dbef40648046
JereEG <jeremiasgonzalez7464@gmail.com>
2023-09-07 17:28:45 -0300

    cambios visueles en control de usuarios

--- gymsy/UserControls/AddClientUserControl.Designer.cs
+++ gymsy/UserControls/AddClientUserControl.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            panel1 = new Panel();
+            a = new Label();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            panel4 = new Panel();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -36,20 +47,124 @@
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(200, 166);
+            label1.Location = new Point(351, 232);
             label1.Name = "label1";
             label1.Size = new Size(277, 37);
             label1.TabIndex = 0;
             label1.Text = "AddClientUserControl";
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(a);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel4);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 530);
+            panel1.TabIndex = 1;
+            // 
+            // a
+            // 
+            a.Anchor = AnchorStyles.Top;
+            a.AutoSize = true;
+            a.Font = new Font("Segoe UI", 29F, FontStyle.Regular, GraphicsUnit.Point);
+            a.ForeColor = SystemColors.ActiveCaptionText;
+            a.Location = new Point(351, 17);
+            a.Name = "a";
+            a.Size = new Size(263, 52);
+            a.TabIndex = 33;
+            a.Text = "Nuevo Cliente";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top;
+            button1.BackColor = Color.DarkOrange;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(677, 122);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 34);
+            button1.TabIndex = 35;
+            button1.Text = "Editar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top;
+            pictureBox1.Image = Properties.Resources.gorilla_avatar;
+            pictureBox1.Location = new Point(677, 48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(69, 68);
+            pictureBox1.TabIndex = 34;
+            pictureBox1.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.Anchor = AnchorStyles.Top;
+            panel4.Controls.Add(textBox4);
+            panel4.Controls.Add(label7);
+            panel4.Controls.Add(label8);
+            panel4.Location = new Point(55, 157);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(340, 88);
+            panel4.TabIndex = 37;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.None;
+            textBox4.Cursor = Cursors.Hand;
+            textBox4.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.ForeColor = Color.FromArgb(9, 0, 20);
+            textBox4.Location = new Point(70, 37);
+            textBox4.Margin = new Padding(5);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(335, 40);
+            textBox4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.None;
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(72, 12);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 25);
+            label7.TabIndex = 1;
+            label7.Text = "Nombre";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.FlatStyle = FlatStyle.Flat;
+            label8.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.Crimson;
+            label8.Location = new Point(255, 22);
+            label8.Name = "label8";
+            label8.Size = new Size(82, 19);
+            label8.TabIndex = 29;
+            label8.Text = "*requerido";
+            // 
             // AddClientUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            Controls.Add(panel1);
             Controls.Add(label1);
             Margin = new Padding(0);
             Name = "AddClientUserControl";
-            Size = new Size(700, 400);
+            Size = new Size(1003, 533);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +172,13 @@
         #endregion
 
         private Label label1;
+        private Panel panel1;
+        private Label a;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Panel panel4;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label8;
     }
 }
--- gymsy/UserControls/ClientsUserControl.Designer.cs
+++ gymsy/UserControls/ClientsUserControl.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             label1 = new Label();
-            dataGridView1 = new DataGridView();
+            driedUsers = new DataGridView();
+            nickname = new DataGridViewTextBoxColumn();
+            rol = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            data_fisic = new DataGridViewTextBoxColumn();
+            last_expiration = new DataGridViewTextBoxColumn();
+            plan = new DataGridViewTextBoxColumn();
+            wallet = new DataGridViewTextBoxColumn();
             textBox1 = new TextBox();
             panel1 = new Panel();
-            button2 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel2 = new Panel();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            BAddUser = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)driedUsers).BeginInit();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -47,62 +59,152 @@
             label1.TabIndex = 0;
             label1.Text = "ClientuserControl";
             // 
-            // dataGridView1
+            // driedUsers
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(23, 120);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(961, 385);
-            dataGridView1.TabIndex = 1;
+            driedUsers.AllowUserToAddRows = false;
+            driedUsers.AllowUserToDeleteRows = false;
+            driedUsers.AllowUserToResizeColumns = false;
+            driedUsers.AllowUserToResizeRows = false;
+            driedUsers.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            driedUsers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            driedUsers.BackgroundColor = Color.FromArgb(224, 224, 224);
+            driedUsers.BorderStyle = BorderStyle.None;
+            driedUsers.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = Color.White;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            driedUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            driedUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            driedUsers.Columns.AddRange(new DataGridViewColumn[] { nickname, rol, name, data_fisic, last_expiration, plan, wallet });
+            driedUsers.EditMode = DataGridViewEditMode.EditProgrammatically;
+            driedUsers.Location = new Point(3, 120);
+            driedUsers.Name = "driedUsers";
+            driedUsers.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            driedUsers.RowHeadersVisible = false;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = Color.DarkOrange;
+            dataGridViewCellStyle4.SelectionForeColor = Color.White;
+            driedUsers.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            driedUsers.RowTemplate.Height = 25;
+            driedUsers.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            driedUsers.ShowCellErrors = false;
+            driedUsers.Size = new Size(994, 400);
+            driedUsers.TabIndex = 1;
+            // 
+            // nickname
+            // 
+            nickname.HeaderText = "Apodo";
+            nickname.Name = "nickname";
+            // 
+            // rol
+            // 
+            rol.HeaderText = "Rol";
+            rol.Name = "rol";
+            // 
+            // name
+            // 
+            name.HeaderText = "Nombre";
+            name.Name = "name";
+            // 
+            // data_fisic
+            // 
+            data_fisic.HeaderText = "Datos Fisicos";
+            data_fisic.Name = "data_fisic";
+            // 
+            // last_expiration
+            // 
+            last_expiration.HeaderText = "Ultimo Vencimiento";
+            last_expiration.Name = "last_expiration";
+            // 
+            // plan
+            // 
+            plan.HeaderText = "Plan";
+            plan.Name = "plan";
+            // 
+            // wallet
+            // 
+            wallet.HeaderText = "Billetera";
+            wallet.Name = "wallet";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(644, 39);
+            textBox1.Location = new Point(40, 42);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(212, 29);
             textBox1.TabIndex = 2;
             // 
             // panel1
             // 
-            panel1.Controls.Add(textBox1);
-            panel1.Controls.Add(button2);
-            panel1.Dock = DockStyle.Fill;
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.AutoSize = true;
+            panel1.Controls.Add(panel2);
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(1000, 530);
             panel1.TabIndex = 4;
             // 
-            // button2
+            // panel2
             // 
-            button2.Anchor = AnchorStyles.Top;
-            button2.BackColor = Color.DarkOrange;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(875, 27);
-            button2.Name = "button2";
-            button2.Padding = new Padding(10);
-            button2.Size = new Size(98, 50);
-            button2.TabIndex = 11;
-            button2.Text = "Buscar";
-            button2.UseVisualStyleBackColor = false;
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Controls.Add(BAddUser);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(iconButton1);
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1000, 114);
+            panel2.TabIndex = 4;
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.DarkOrange;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.ForeColor = Color.Transparent;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(258, 34);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(77, 47);
+            iconButton1.TabIndex = 3;
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // BAddUser
+            // 
+            BAddUser.BackColor = Color.FromArgb(0, 192, 0);
+            BAddUser.FlatStyle = FlatStyle.Flat;
+            BAddUser.ForeColor = Color.Transparent;
+            BAddUser.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+            BAddUser.IconColor = Color.White;
+            BAddUser.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            BAddUser.IconSize = 60;
+            BAddUser.Location = new Point(390, 29);
+            BAddUser.Name = "BAddUser";
+            BAddUser.Size = new Size(72, 56);
+            BAddUser.TabIndex = 4;
+            BAddUser.UseVisualStyleBackColor = false;
             // 
             // ClientsUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(label1);
-            Controls.Add(dataGridView1);
+            Controls.Add(driedUsers);
             Controls.Add(panel1);
             Margin = new Padding(0);
             Name = "ClientsUserControl";
             Size = new Size(1000, 530);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)driedUsers).EndInit();
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -110,9 +212,18 @@
         #endregion
 
         private Label label1;
-        private DataGridView dataGridView1;
+        private DataGridView driedUsers;
         private TextBox textBox1;
         private Panel panel1;
-        private Button button2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private DataGridViewTextBoxColumn nickname;
+        private DataGridViewTextBoxColumn rol;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn data_fisic;
+        private DataGridViewTextBoxColumn last_expiration;
+        private DataGridViewTextBoxColumn plan;
+        private DataGridViewTextBoxColumn wallet;
+        private Panel panel2;
+        private FontAwesome.Sharp.IconButton BAddUser;
     }
 }
