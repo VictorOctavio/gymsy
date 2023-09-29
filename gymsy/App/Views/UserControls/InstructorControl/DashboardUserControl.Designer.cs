12b87ee7c887155c874f484e2000cbbf00950714
JereEG <jeremiasgonzalez7464@gmail.com>
2023-09-28 22:05:02 -0300

    se añadiron archivos al gitignore

--- gymsy/App/Views/UserControls/InstructorControl/DashboardUserControl.Designer.cs
+++ gymsy/App/Views/UserControls/InstructorControl/DashboardUserControl.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            LModoBusqueda = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel5.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -43,15 +53,88 @@
             label1.TabIndex = 1;
             label1.Text = "DashboardUserControl";
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(16, 8, 23);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 530);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(LModoBusqueda);
+            panel2.Location = new Point(48, 261);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(289, 269);
+            panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(3, 34);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(522, 221);
+            panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(541, 34);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(405, 221);
+            panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(label2);
+            panel5.Location = new Point(343, 261);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(603, 266);
+            panel5.TabIndex = 1;
+            // 
+            // LModoBusqueda
+            // 
+            LModoBusqueda.AutoSize = true;
+            LModoBusqueda.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            LModoBusqueda.ForeColor = Color.FromArgb(230, 237, 243);
+            LModoBusqueda.Location = new Point(82, 9);
+            LModoBusqueda.Name = "LModoBusqueda";
+            LModoBusqueda.Size = new Size(102, 25);
+            LModoBusqueda.TabIndex = 84;
+            LModoBusqueda.Text = "Top Planes";
+            LModoBusqueda.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(230, 237, 243);
+            label2.Location = new Point(166, 7);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 25);
+            label2.TabIndex = 85;
+            label2.Text = "Transacciones";
+            label2.Visible = false;
+            // 
             // DashboardUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Transparent;
+            Controls.Add(panel1);
             Controls.Add(label1);
             Margin = new Padding(0);
             Name = "DashboardUserControl";
             Size = new Size(1000, 530);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -59,5 +142,12 @@
         #endregion
 
         private Label label1;
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private Panel panel5;
+        private Panel panel4;
+        private Label label2;
+        private Label LModoBusqueda;
     }
 }
