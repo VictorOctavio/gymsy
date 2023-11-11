3a1ca63ceee6b2bc31d3f9e57a34a7207bc16665
JereEG <jeremiasgonzalez7464@gmail.com>
2023-11-11 01:24:14 -0300

    restore  en adminControl

--- gymsy/App/Views/UserControls/AdminControls/adminControl.Designer.cs
+++ gymsy/App/Views/UserControls/AdminControls/adminControl.Designer.cs
@@ -29,15 +29,23 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            label20 = new Label();
+            TBRutaArchivo = new TextBox();
             BGuardarBackUp = new ClassCustom.RJButton();
             label1 = new Label();
             openFileDialog1 = new OpenFileDialog();
+            BBuscarArchivo = new ClassCustom.RJButton();
+            label2 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(16, 8, 23);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(BBuscarArchivo);
+            panel1.Controls.Add(label20);
+            panel1.Controls.Add(TBRutaArchivo);
             panel1.Controls.Add(BGuardarBackUp);
             panel1.Controls.Add(label1);
             panel1.Dock = DockStyle.Fill;
@@ -46,6 +54,30 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(1000, 530);
             panel1.TabIndex = 0;
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label20.ForeColor = Color.FromArgb(230, 237, 243);
+            label20.Location = new Point(378, 119);
+            label20.Name = "label20";
+            label20.Size = new Size(236, 21);
+            label20.TabIndex = 92;
+            label20.Text = "Ruta de restauracion con backup";
+            // 
+            // TBRutaArchivo
+            // 
+            TBRutaArchivo.BackColor = Color.FromArgb(69, 34, 99);
+            TBRutaArchivo.BorderStyle = BorderStyle.None;
+            TBRutaArchivo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            TBRutaArchivo.ForeColor = Color.FromArgb(230, 237, 243);
+            TBRutaArchivo.Location = new Point(390, 156);
+            TBRutaArchivo.Name = "TBRutaArchivo";
+            TBRutaArchivo.PlaceholderText = "...";
+            TBRutaArchivo.ReadOnly = true;
+            TBRutaArchivo.Size = new Size(186, 22);
+            TBRutaArchivo.TabIndex = 91;
             // 
             // BGuardarBackUp
             // 
@@ -63,12 +95,12 @@
             BGuardarBackUp.IconFont = FontAwesome.Sharp.IconFont.Auto;
             BGuardarBackUp.IconSize = 45;
             BGuardarBackUp.ImageAlign = ContentAlignment.MiddleRight;
-            BGuardarBackUp.Location = new Point(203, 176);
+            BGuardarBackUp.Location = new Point(139, 173);
             BGuardarBackUp.Name = "BGuardarBackUp";
             BGuardarBackUp.Padding = new Padding(10, 0, 0, 0);
-            BGuardarBackUp.Size = new Size(145, 56);
+            BGuardarBackUp.Size = new Size(160, 65);
             BGuardarBackUp.TabIndex = 90;
-            BGuardarBackUp.Text = "Guardar";
+            BGuardarBackUp.Text = "Crear Nueva Copia";
             BGuardarBackUp.TextAlign = ContentAlignment.MiddleLeft;
             BGuardarBackUp.TextColor = Color.White;
             BGuardarBackUp.UseVisualStyleBackColor = false;
@@ -78,7 +110,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(180, 45);
+            label1.Location = new Point(121, 73);
             label1.Name = "label1";
             label1.Size = new Size(202, 30);
             label1.TabIndex = 0;
@@ -87,6 +119,38 @@
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BBuscarArchivo
+            // 
+            BBuscarArchivo.BackColor = Color.DarkOrange;
+            BBuscarArchivo.BackgroundColor = Color.DarkOrange;
+            BBuscarArchivo.BorderColor = Color.PaleVioletRed;
+            BBuscarArchivo.BorderRadius = 25;
+            BBuscarArchivo.BorderSize = 0;
+            BBuscarArchivo.FlatAppearance.BorderSize = 0;
+            BBuscarArchivo.FlatStyle = FlatStyle.Flat;
+            BBuscarArchivo.ForeColor = Color.White;
+            BBuscarArchivo.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            BBuscarArchivo.IconColor = Color.White;
+            BBuscarArchivo.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            BBuscarArchivo.IconSize = 30;
+            BBuscarArchivo.Location = new Point(582, 143);
+            BBuscarArchivo.Name = "BBuscarArchivo";
+            BBuscarArchivo.Size = new Size(50, 50);
+            BBuscarArchivo.TabIndex = 93;
+            BBuscarArchivo.TextColor = Color.White;
+            BBuscarArchivo.UseVisualStyleBackColor = false;
+            BBuscarArchivo.Click += BBuscarArchivo_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(378, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(269, 30);
+            label2.TabIndex = 94;
+            label2.Text = "Buscar copia de seguridad";
             // 
             // adminControl
             // 
@@ -106,5 +170,9 @@
         private Label label1;
         private ClassCustom.RJButton BGuardarBackUp;
         private OpenFileDialog openFileDialog1;
+        private TextBox TBRutaArchivo;
+        private Label label20;
+        private ClassCustom.RJButton BBuscarArchivo;
+        private Label label2;
     }
 }
--- gymsy/App/Views/UserControls/AdminControls/adminControl.cs
+++ gymsy/App/Views/UserControls/AdminControls/adminControl.cs
@@ -55,5 +55,87 @@ namespace gymsy.App.Views.UserControls.AdminControls
             }
 
         }
+
+        private void BRestaurarDB_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TBRutaArchivo.Text))
+            {
+                MessageBox.Show("Por favor, cargue la ruta del archivo.");
+            }
+            else
+            {
+                this.RealizarRestauracion(TBRutaArchivo.Text);
+            }
+        }
+
+        public void RealizarRestauracion(string backupPath)
+        {
+            string databaseName;
+            string connectionString;
+
+            using (var context = this.dbContext)
+            {
+                connectionString = Resources.stringConnection;
+                databaseName = "gymsy";
+            }
+
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlsingleuser = $"ALTER DATABASE {databaseName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                    using (SqlCommand singleusercommand = new SqlCommand(sqlsingleuser, connection))
+                    {
+                        singleusercommand.ExecuteNonQuery();
+                    }
+                    string sqlUseMaster = "USE master;";
+                    using (SqlCommand useMasterCommand = new SqlCommand(sqlUseMaster, connection))
+                    {
+                        useMasterCommand.ExecuteNonQuery();
+                    }
+                    string sqlRestore = $"RESTORE DATABASE {databaseName} FROM DISK = '{backupPath}';";
+
+                    using (SqlCommand restoreCommand = new SqlCommand(sqlRestore, connection))
+                    {
+                        restoreCommand.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Restauración completada con éxito.");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error al restaurar la base de datos: " + ex.Message);
+            }
+        }
+
+        private void BBuscarArchivo_Click_1(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos .bak|*.bak";
+
+            // Muestra el cuadro de diálogo para seleccionar un archivo .bak
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    string rutaArchivo = openFileDialog.FileName;
+
+
+                    // Muestra el contenido en el TextBox
+                    TBRutaArchivo.Text = rutaArchivo;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error al cargar el archivo .bak: " + ex.Message);
+                }
+            }
+        }
     }
 }
